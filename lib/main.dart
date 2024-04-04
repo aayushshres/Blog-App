fe59d415d359b19fe2a07a62ade171c724bb09ea
Aayush Shrestha <aayushshres20@gmail.com>
2024-04-04 17:07:57 +0530

    complete auth features

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
@@ -15,11 +16,14 @@ void main() async {
       builder: (context) => MultiBlocProvider(
         providers: [
           BlocProvider(
+            create: (_) => serviceLocator<AppUserCubit>(),
+          ),
+          BlocProvider(
             create: (_) => serviceLocator<AuthBloc>(),
           ),
         ],
         child: const MyApp(),
-      ), // Wrap your app
+      ),
     ),
   );
 }
@@ -44,7 +48,21 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: AppTheme.darkThemeMode,
-      home: const LoginPage(),
+      home: BlocSelector<AppUserCubit, AppUserState, bool>(
+        selector: (state) {
+          return state is AppUserLoggedIn;
+        },
+        builder: (context, isLoggedIn) {
+          if (isLoggedIn) {
+            return const Scaffold(
+              body: Center(
+                child: Text("Logged in"),
+              ),
+            );
+          }
+          return const LoginPage();
+        },
+      ),
     );
   }
 }
